5e255137d307c377fa2df9a3cf49b8bb8cd9131f
Paulo Sims <paulo@apolloagriculture.com>
2023-08-23 19:32:20 +0100

    updated test to use IO

--- src/test/scala/paulo/baena/todo/api/RoutesSpec.scala
+++ src/test/scala/paulo/baena/todo/api/RoutesSpec.scala
@@ -1,46 +1,56 @@
 package paulo.baena.todo.api
 
+import cats.effect.{Clock, IO}
 import cats.data.OptionT
-import cats.effect.*
 import cats.implicits.*
 import org.http4s.*
+import org.http4s.implicits.*
+import org.http4s.dsl.io.*
 import org.http4s.circe.*
 import io.circe.generic.auto.*
 import io.circe.{Decoder, Encoder}
 import munit.CatsEffectSuite
+import paulo.baena.todo.api.Routes
 import paulo.baena.todo.api.Messages.*
-import paulo.baena.todo.persistence.Representations.TodoItem
+import paulo.baena.todo.persistence.Representations.{CreateTodoCommand, TodoItem, UpdateTodoCommand}
 import paulo.baena.todo.persistence.TestTodoRepository
 
-import java.time.OffsetDateTime
+import java.time.{Instant, OffsetDateTime, ZoneOffset}
+import scala.concurrent.duration.*
 
-class RoutesSpec[F[_]: Async] extends CatsEffectSuite {
+class RoutesSpec extends CatsEffectSuite {
 
-  val testRepo: TestTodoRepository[F] = TestTodoRepository.inMemory[F]
+  val testRepo: TestTodoRepository[IO] = TestTodoRepository.inMemory
 
-  val routes: HttpRoutes[F] = Routes.live(testRepo)
+  val routes: HttpRoutes[IO] = Routes.live(testRepo)
 
-  implicit def circeJsonDecoder[A: Decoder]: EntityDecoder[F, A] = jsonOf[F, A]
+  implicit def circeJsonDecoder[A: Decoder]: EntityDecoder[IO, A] = jsonOf[IO, A]
 
-  implicit def circeJsonEncoder[A: Encoder]: EntityEncoder[F, A] = jsonEncoderOf[F, A]
+  implicit def circeJsonEncoder[A: Encoder]: EntityEncoder[IO, A] = jsonEncoderOf[IO, A]
 
-  def getRequest(uri: String)(implicit F: Sync[F]): F[Response[F]] =
-    routes.run(Request[F](Method.GET, Uri.unsafeFromString(uri))).value.flatMap {
-      case Some(response) => F.pure(response)
-      case None           => F.raiseError(new RuntimeException("Response not found"))
-    }
+  def getRequest(uri: String): IO[Response[IO]] =
+    routes.run(Request[IO](Method.GET, Uri.unsafeFromString(uri)))
+      .value
+      .flatMap {
+        case Some(response) => IO.pure(response)
+        case None => IO.raiseError(new RuntimeException("Response not found"))
+      }
 
-  def deleteRequest(uri: String)(implicit F: Sync[F]): F[Response[F]] =
-    routes.run(Request[F](Method.DELETE, Uri.unsafeFromString(uri))).value.flatMap {
-      case Some(response) => F.pure(response)
-      case None           => F.raiseError(new RuntimeException("Response not found"))
-    }
+  def deleteRequest(uri: String): IO[Response[IO]] =
+    routes.run(Request[IO](Method.DELETE, Uri.unsafeFromString(uri)))
+      .value
+      .flatMap {
+        case Some(response) => IO.pure(response)
+        case None => IO.raiseError(new RuntimeException("Response not found"))
+      }
 
-  def getResponse(maybeResponse: OptionT[F, Response[F]])(implicit F: Sync[F]): F[Response[F]] =
-    maybeResponse.value.flatMap {
-      case Some(response) => F.pure(response)
-      case None           => F.raiseError(new RuntimeException("Response not found"))
-    }
+  def getResponse(maybeResponse: OptionT[IO, Response[IO]]): IO[Response[IO]] =
+    maybeResponse
+      .value
+      .flatMap {
+        case Some(response) => IO.pure(response)
+        case None => IO.raiseError(new RuntimeException("Response not found"))
+      }
 
   private val root = Uri.unsafeFromString("/")
 
@@ -48,10 +58,12 @@ class RoutesSpec[F[_]: Async] extends CatsEffectSuite {
     val newTodo = CreateTodoRequest("New Todo", 0)
 
     for {
-      response    <- getResponse(routes.run(Request[F](Method.POST, root).withEntity(newTodo)))
+      response <- getResponse(routes.run(Request[IO](Method.POST, root).withEntity(newTodo)))
       createdTodo <- response.as[TodoItem]
-    } yield assertEquals(response.status, Status.Created)
-    // Add more assertions as needed
+    } yield {
+      assertEquals(response.status, Status.Created)
+      // Add more assertions as needed
+    }
   }
 
   test("GET: Trying to get a TODO that doesn't exist") {
@@ -59,7 +71,9 @@ class RoutesSpec[F[_]: Async] extends CatsEffectSuite {
 
     for {
       response <- getRequest(s"/$nonExistingId")
-    } yield assertEquals(response.status, Status.NotFound)
+    } yield {
+      assertEquals(response.status, Status.NotFound)
+    }
   }
 
   test("GET: Getting all todos") {
@@ -67,10 +81,10 @@ class RoutesSpec[F[_]: Async] extends CatsEffectSuite {
     val newTodo2 = CreateTodoRequest("Todo 2", 1)
 
     for {
-      _        <- getResponse(routes.run(Request[F](Method.POST, root).withEntity(newTodo1)))
-      _        <- getResponse(routes.run(Request[F](Method.POST, root).withEntity(newTodo2)))
+      _ <- getResponse(routes.run(Request[IO](Method.POST, root).withEntity(newTodo1)))
+      _ <- getResponse(routes.run(Request[IO](Method.POST, root).withEntity(newTodo2)))
       response <- getRequest("/")
-      todos    <- response.as[List[TodoItem]]
+      todos <- response.as[List[TodoItem]]
     } yield {
       assertEquals(response.status, Status.Ok)
       assertEquals(todos.length, 2)
@@ -78,14 +92,14 @@ class RoutesSpec[F[_]: Async] extends CatsEffectSuite {
   }
 
   test("PATCH: Updating that todo") {
-    val newTodo    = CreateTodoRequest("New Todo", 0)
+    val newTodo = CreateTodoRequest("New Todo", 0)
     val updateTodo = UpdateTodoRequest(Some("Updated Title"), None, None)
 
     for {
-      postResponse  <- getResponse(routes.run(Request[F](Method.POST, root).withEntity(newTodo)))
-      createdTodo   <- postResponse.as[TodoItem]
-      patchResponse <- getResponse(routes.run(Request[F](Method.PATCH, root).withEntity(updateTodo)))
-      updatedTodo   <- patchResponse.as[TodoItem]
+      postResponse <- getResponse(routes.run(Request[IO](Method.POST, root).withEntity(newTodo)))
+      createdTodo <- postResponse.as[TodoItem]
+      patchResponse <- getResponse(routes.run(Request[IO](Method.PATCH, root).withEntity(updateTodo)))
+      updatedTodo <- patchResponse.as[TodoItem]
     } yield {
       assertEquals(patchResponse.status, Status.Ok)
       assertEquals(updatedTodo.title, "Updated Title")
@@ -94,11 +108,13 @@ class RoutesSpec[F[_]: Async] extends CatsEffectSuite {
 
   test("PATCH: Trying to update a todo that doesn't exist") {
     val nonExistingId = Long.MaxValue
-    val updateTodo    = UpdateTodoRequest(Some("Updated Title"), None, None)
+    val updateTodo = UpdateTodoRequest(Some("Updated Title"), None, None)
 
     for {
-      response <- getResponse(routes.run(Request[F](Method.PATCH, root).withEntity(updateTodo)))
-    } yield assertEquals(response.status, Status.NotFound)
+      response <- getResponse(routes.run(Request[IO](Method.PATCH, root).withEntity(updateTodo)))
+    } yield {
+      assertEquals(response.status, Status.NotFound)
+    }
   }
 
   test("DELETE: Trying to delete a non-existing todo") {
@@ -106,7 +122,9 @@ class RoutesSpec[F[_]: Async] extends CatsEffectSuite {
 
     for {
       response <- deleteRequest(s"/$nonExistingId")
-    } yield assertEquals(response.status, Status.NotFound)
+    } yield {
+      assertEquals(response.status, Status.NotFound)
+    }
   }
 
   test("DELETE all TODOs") {
@@ -114,11 +132,11 @@ class RoutesSpec[F[_]: Async] extends CatsEffectSuite {
     val newTodo2 = CreateTodoRequest("Todo 2", 1)
 
     for {
-      _              <- getResponse(routes.run(Request[F](Method.POST, root).withEntity(newTodo1)))
-      _              <- getResponse(routes.run(Request[F](Method.POST, root).withEntity(newTodo2)))
+      _ <- getResponse(routes.run(Request[IO](Method.POST, root).withEntity(newTodo1)))
+      _ <- getResponse(routes.run(Request[IO](Method.POST, root).withEntity(newTodo2)))
       deleteResponse <- deleteRequest("/")
       getAllResponse <- getRequest("/")
-      todos          <- getAllResponse.as[List[TodoItem]]
+      todos <- getAllResponse.as[List[TodoItem]]
     } yield {
       assertEquals(deleteResponse.status, Status.Ok)
       assertEquals(todos.length, 0)
