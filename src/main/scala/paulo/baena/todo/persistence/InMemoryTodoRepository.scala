bc2374f6df10bf66dce6ff0399b3a2390239b48c
Paulo Sims <paulo@apolloagriculture.com>
2023-08-24 23:38:38 +0100

    implement queries

--- src/main/scala/paulo/baena/todo/persistence/InMemoryTodoRepository.scala
+++ src/main/scala/paulo/baena/todo/persistence/InMemoryTodoRepository.scala
@@ -1,20 +1,66 @@
 package paulo.baena.todo.persistence
 
 import cats.effect.Async
-import doobie.Transactor
-import paulo.baena.todo.config.DatabaseConfig
-import paulo.baena.todo.persistence.Representations.TodoItem
+import cats.implicits.*
+import doobie.*
+import doobie.implicits.*
+import paulo.baena.todo.persistence.Representations.*
 
 final case class InMemoryTodoRepository[F[_]: Async](transactor: Transactor[F]) extends TodoRepository[F] {
-  def createTodo(createTodo: Representations.CreateTodoCommand): F[TodoItem] = ???
+  def createTodo(createTodo: CreateTodoCommand): F[TodoItem] =
+    sql"""
+         INSERT INTO todos (title, order, completed)
+         VALUES (${createTodo.title}, ${createTodo.order}, FALSE)
+         RETURNING id, title, order, completed, updated_at, created_at
+       """
+      .query[TodoItem]
+      .unique
+      .transact(transactor)
 
-  def getTodoById(todoId: Long): F[Option[TodoItem]] = ???
+  def getTodoById(todoId: Long): F[Option[TodoItem]] =
+    sql"""
+         SELECT id, title, order, completed, updated_at, created_at
+         FROM todos
+         WHERE id = $todoId
+         """
+      .query[TodoItem]
+      .option
+      .transact(transactor)
 
-  def getAll: F[List[TodoItem]] = ???
+  def getAll: F[List[TodoItem]] =
+    sql"""
+         SELECT id, title, order, completed, updated_at, created_at
+         FROM todos
+         """
+      .query[TodoItem]
+      .to[List]
+      .transact(transactor)
 
-  def deleteTodoById(todoId: Long): F[Option[Unit]] = ???
+  def deleteTodoById(todoId: Long): F[Option[Unit]] =
+    sql"""
+         DELETE FROM todos
+         WHERE id = $todoId
+         """.update.run
+      .transact(transactor)
+      .map {
+        case 0 => None
+        case _ => ().some
+      }
 
-  def deleteAll: F[Unit] = ???
+  def deleteAll: F[Unit] =
+    sql"""
+         DELETE FROM todos
+         """.update.run.void
+      .transact(transactor)
 
-  def updateTodo(todoId: Long, updateTodo: Representations.UpdateTodoCommand): F[Option[TodoItem]] = ???
+  def updateTodo(todoId: Long, updateTodo: UpdateTodoCommand): F[Option[TodoItem]] =
+    sql"""
+         UPDATE todos
+         SET (title = ${updateTodo.title}, order = ${updateTodo.order}, completed = ${updateTodo.completed})
+         WHERE id = $todoId
+         RETURNING id, title, order, completed, updated_at, created_at
+       """
+      .query[TodoItem]
+      .option
+      .transact(transactor)
 }
--- src/main/scala/paulo/baena/todo/persistence/Representations.scala
+++ src/main/scala/paulo/baena/todo/persistence/Representations.scala
@@ -1,5 +1,7 @@
 package paulo.baena.todo.persistence
 
+import doobie._
+import doobie.implicits.javasql.TimestampMeta
 import io.scalaland.chimney.dsl.*
 import paulo.baena.todo.api.Messages.TodoItemResponse
 
@@ -15,6 +17,17 @@ object Representations {
     createdAt: OffsetDateTime
   ) {
     lazy val asTodoItemResponse = this.transformInto[TodoItemResponse]
+  }
+
+  // TODO: doobie type classes should live here or on the repo?
+  implicit val offsetDateTimeMeta: Meta[OffsetDateTime] =
+    Meta[java.sql.Timestamp].imap(ts => ts.toInstant.atOffset(OffsetDateTime.now().getOffset))(odt =>
+      java.sql.Timestamp.from(odt.toInstant)
+    )
+
+  implicit val todoItemRead: Read[TodoItem] = Read[(Long, String, Int, Boolean, OffsetDateTime, OffsetDateTime)].map {
+    case (id, title, order, completed, updatedAt, createdAt) =>
+      TodoItem(id, title, order, completed, updatedAt, createdAt)
   }
 
   final case class CreateTodoCommand(title: String, order: Int)
