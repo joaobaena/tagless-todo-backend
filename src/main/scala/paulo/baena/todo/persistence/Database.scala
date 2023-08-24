228e7ef5f07cac558d4c687ee1ebda53ab0c0ebf
Paulo Sims <paulo@apolloagriculture.com>
2023-08-24 22:51:15 +0100

    some todos

--- src/main/scala/paulo/baena/todo/persistence/Database.scala
+++ src/main/scala/paulo/baena/todo/persistence/Database.scala
@@ -9,6 +9,7 @@ import paulo.baena.todo.config.DatabaseConfig
 object Database {
   def transactor[F[_]: Async](config: DatabaseConfig): Resource[F, HikariTransactor[F]] =
     for {
+      // TODO: Check how to use CE3 executors
       ce <- ExecutionContexts.fixedThreadPool[F](4)
       xa <- HikariTransactor.newHikariTransactor[F](
               driverClassName = config.driver,
