c2d576af6635b54eced3c8762c9b6185d1e7bc8d
Paulo Sims <paulo@apolloagriculture.com>
2023-08-25 22:54:30 +0100

    add cors

--- src/main/scala/paulo/baena/todo/Main.scala
+++ src/main/scala/paulo/baena/todo/Main.scala
@@ -3,11 +3,13 @@ package paulo.baena.todo
 import cats.effect.*
 import com.comcast.ip4s.*
 import org.http4s.ember.server.*
+import org.http4s.server.middleware.{CORS, CORSConfig}
 import paulo.baena.todo.api.Routes
 import paulo.baena.todo.config.ApplicationConfig
 import paulo.baena.todo.persistence.*
 
 object Main extends IOApp {
+  @scala.annotation.nowarn("msg=.*CORSConfig.*")
   def run(args: List[String]): IO[ExitCode] = {
     val configLoader   = ApplicationConfig.configLoader[IO]
     val serverResource =
@@ -19,11 +21,14 @@ object Main extends IOApp {
         transactor       <- database.transactor
         repository        = InMemoryTodoRepository[IO](transactor)
         httpApp           = Routes.live(repository).orNotFound
+        config            = CORSConfig.default
+                              .withAnyOrigin(false)
+                              .withAllowedOrigins(Set("http://www.todobackend.com"))
         server           <- EmberServerBuilder
                               .default[IO]
                               .withHost(Host.fromString(httpServerConfig.url).get)
                               .withPort(Port.fromInt(httpServerConfig.port).get)
-                              .withHttpApp(httpApp)
+                              .withHttpApp(CORS(httpApp, config))
                               .build
       } yield server
     serverResource
